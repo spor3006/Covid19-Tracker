cb07edde30d96fcd7c6d1afca39701b9cfd11d96
spor3006 <41056517+spor3006@users.noreply.github.com>
2020-05-24 14:38:09 +0530

    Almost Done

--- src/App.js
+++ src/App.js
@@ -1,16 +1,40 @@
-import React, { Component } from "react";
-import "./styles.css";
-import Navbar from "./components/Navbar";
+import React , {Component} from "react";
+
+import styles from './App.module.css'
+
+import { Cards, Chart, CountryPicker } from "./components"
+import { fetchData } from "./api"
+
+import coronaImage from './images/image.png'
+
 class App extends Component {
-  render() {
+  state = {
+    data :{},
+    country:'',
+  }
+  handleCountryChange = async (country) => {
+    const fetchedData = await fetchData(country)
+    this.setState({data:fetchedData , country:country})
+  }
+  async componentDidMount(){
+    const fetchedData = await fetchData();
+    this.setState({data : fetchedData })
+    
+  }
+  render(){
+    const {data , country} = this.state 
     return (
-      <div className="App">
-        <Navbar />
-        <h1>Hello CodeSandbox</h1>
-        <h2>Start editing to see some magic happen!</h2>
+      
+      <div className={styles.container}>
+        <img className = {styles.image} src={coronaImage} alt=""/>
+        <Cards data = {data}/> 
+        <CountryPicker handleCountryChange= {this.handleCountryChange}/>
+        <br></br>
+        <Chart data={data} country ={country}/>
       </div>
     );
   }
 }
+ 
 
 export default App;
